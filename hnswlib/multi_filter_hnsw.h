7e07038a4ed94e8f97a2ed0c4ee9cdade96d5c5d
MrPekar98 <martin.pekar@outlook.dk>
2025-12-10 16:18:44 +0100

    Implemented multi index baseline

---
+++ hnswlib/multi_filter_hnsw.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "hnswlib.h"
+#include "hnswalg.h"
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <fstream>
+#include <vector>
+
+namespace hnswlib
+{
+	template<typename dist_t>
+	class MultiIndexHNSW: public AlgorithmInterface<dist_t>
+	{
+	private:
+		SpaceInterface<dist_t> *s;
+		std::string location;
+		bool nmslib, allow_replace_deleted;
+		size_t max_elements, M, ef_construction, random_seed;
+		std::unordered_map<std::string, unsigned> lookup;
+		std::vector<HierarchicalNSW<dist_t>*> indexes;
+
+	public:
+		explicit MultiIndexHNSW(SpaceInterface<dist_t> *s)
+			: s(s), nmslib(false), max_elements(max_elements), allow_replace_deleted(false),
+				M(16), ef_construction(200), random_seed(100)
+		{}
+
+		MultiIndexHNSW(
+			SpaceInterface<dist_t> *s,
+            const std::string &location,
+            bool nmslib = false,
+            size_t max_elements = 0,
+            bool allow_replace_deleted = false)
+			: s(s), location(s), nmslib(nmslib), max_elements(max_elements), allow_replace_deleted(allow_replace_deleted),
+				M(16), ef_construction(200), random_seed(100)
+		{}
+
+		MultiIndexHNSW(
+			SpaceInterface<dist_t> *s,
+            size_t max_elements,
+            size_t M = 16,
+            size_t ef_construction = 200,
+            size_t random_seed = 100,
+            bool allow_replace_deleted = false)
+			: s(s), nmslib(false), max_elements(max_elements), allow_replace_deleted(allow_replace_deleted),
+				M(M), ef_construction(ef_construction), random_seed(random_seed)
+		{}
+
+		void addPoint(const void *datapoint, labeltype label, const std::vector<std::string>& tags, bool replace_deleted = false)
+		{
+			for (const std::string& tag : tags)
+			{
+				if (lookup.find(tag) == lookup.end())
+				{
+					lookup.insert({tag, indexes.size()});
+					indexes.push_back(new HierarchicalNSW<dist_t>(s, max_elements, M, ef_construction, random_seed, allow_replace_deleted));
+				}
+
+				unsigned index = lookup[tag];
+				indexes[index]->addPoint(datapoint, label, {}, replace_deleted);
+			}
+		}
+
+		std::priority_queue<std::pair<dist_t, labeltype>> searchKnn(const void *query_data, size_t k, const std::vector<std::string>& tags, BaseFilterFunctor* isIdAllowed = nullptr) const
+		{
+			std::priority_queue<std::pair<dist_t, labeltype>> results;
+			std::unordered_set<labeltype> exists;
+			exists.reserve(k);
+
+			for (const std::string& tag : tags)
+			{
+				if (lookup.find(tag) != lookup.end())
+				{
+					unsigned index = lookup.at(tag);
+					std::priority_queue<std::pair<dist_t, labeltype>> tagResults = indexes[index]->searchKnn(query_data, k, {}, isIdAllowed);
+
+					while (!tagResults.empty())
+					{
+						std::pair<dist_t, labeltype> result = tagResults.top();
+						tagResults.pop();
+
+						if (exists.find(result.second) == exists.end())
+						{
+							results.push(result);
+							exists.insert(result.second);
+						}
+					}
+				}
+			}
+
+			while (results.size() > k)
+			{
+				results.pop();
+			}
+
+			return results;
+		}
+
+		void saveIndex(const std::string& location)
+		{
+			std::ofstream metaStream(location + ".meta");
+			std::unordered_map<unsigned, std::string> inverted;
+
+			for (const auto& pair : lookup)
+			{
+				inverted.insert({pair.second, pair.first});
+			}
+
+			for (unsigned index = 0; index < indexes.size(); index++)
+			{
+				metaStream << inverted[index] << "\n";
+				indexes[index]->saveIndex(location + "." + std::to_string(index));
+			}
+		}
+
+		void loadIndex(const std::string& location, SpaceInterface<dist_t>* s, size_t max_elements_i = 0)
+		{
+			std::ifstream metaStream(location + ".meta");
+			std::string tag;
+			unsigned index = 0;
+
+			while (std::getline(metaStream, tag))
+			{
+				HierarchicalNSW<dist_t> hnsw(s);
+				hnsw.loadIndex(location + "." + std::to_string(index), s, max_elements_i);
+				lookup.insert({tag, index});
+				indexes.push_back(&hnsw);
+			}
+		}
+	};
+}
