3907a97e5cf4b68eec375d65f621e6062998516c
MrPekar98 <martin.pekar@outlook.dk>
2025-12-15 14:45:55 +0000

    Evaluates all baselines

--- evaluation/evaluate_postfilter_baselines.cpp
+++ evaluation/evaluate_postfilter_baselines.cpp
@@ -1,4 +1,6 @@
 #include <postfilter_hnsw.h>
+#include <multi_filter_hnsw.h>
+#include <hnswalg.h>
 #include <string>
 #include <chrono>
 #include <filesystem>
@@ -69,7 +71,7 @@ Query parse(const std::string& queryFile)
 }
 
 // Returns runtimes
-std::vector<int> postFilterBaselineEvaluateQueries(const std::string& queryPath, const std::string& resultsPath, const hnswlib::PostfilterHNSW<float>& baseline, size_t k)
+std::vector<int> evaluateQueries(const std::string& queryPath, const std::string& resultsPath, const hnswlib::AlgorithmInterface<float>* baseline, size_t k)
 {
     std::vector<int> runtimes;
 
@@ -79,7 +81,7 @@ std::vector<int> postFilterBaselineEvaluateQueries(const std::string& queryPath,
         std::string queryName = entry.path().filename().string();
         Query q = parse(queryFile);
         std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
-        std::priority_queue<std::pair<float, labeltype>> results = baseline.searchKnn((void*) q.embedding, k, q.tags);
+        std::priority_queue<std::pair<float, labeltype>> results = baseline->searchKnn((void*) q.embedding, k, q.tags);
         std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - startTime);
         runtimes.push_back(duration.count());
 
@@ -105,80 +107,131 @@ double average(const std::vector<int>& values)
 int main()
 {
     std::string queryDir = "queries/single-tag/", heteroQueries = queryDir + "heterogeneous/", homoQueries = queryDir + "homogeneous/",
-        resultDir = "results/", baselineResultDir = resultDir + "postfilter_baseline/";
-    std::vector<int> allRuntimes;
+        resultDir = "results/", postfilterBaselineResultDir = resultDir + "postfilter_baseline/", multiFilterBaselineResultDir = resultDir + "multifilter_baseline/", adaptiveIndexResultDir = resultDir + "adaptive/";
+    std::vector<int> allPostfilterRuntimes, allMultiRuntimes, allAdaptiveRuntimes;
     size_t max_elements = 32440681;
     size_t k = 100;
     hnswlib::InnerProductSpace space(200);
     hnswlib::PostfilterHNSW<float> postfilterBaseline(&space);
+    hnswlib::MultiIndexHNSW<float> multiIndexBaseline(&space, {});
+    hnswlib::HierarchicalNSW<float> adaptiveHnsw(&space);
+    std::cout << "Loading indexes\n" << std::endl;
     postfilterBaseline.loadIndex("indexes/postfilter_index.idx", &space, max_elements);
+    multiIndexBaseline.loadIndex("indexes/multi_index_index.idx", &space, max_elements);
 
     if (!std::filesystem::exists(resultDir))
     {
         std::filesystem::create_directory(resultDir);
-    }
-
-    if (!std::filesystem::exists(baselineResultDir))
-    {
-        std::filesystem::create_directory(baselineResultDir);
+        std::filesystem::create_directory(postfilterBaselineResultDir);
+        std::filesystem::create_directory(multiFilterBaselineResultDir);
+        std::filesystem::create_directory(adaptiveIndexResultDir);
     }
 
     // Heterogeneous 90th percentile queries
     std::cout << "90th percentile heterogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "hetero_90th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "hetero_90th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "hetero_90th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "hetero_90th-percentile/");
 
-    std::vector<int> runtimes = postFilterBaselineEvaluateQueries(heteroQueries + "90th-percentile/", baselineResultDir + "hetero_90th-percentile/", postfilterBaseline, k);
-    double averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    std::vector<int> postfilterRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", postfilterBaselineResultDir + "hetero_90th-percentile/", &postfilterBaseline, k);
+    std::vector<int> multiRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", multiFilterBaselineResultDir + "hetero_90th-percentile/", &multiIndexBaseline, k);
+    std::vector<int> adaptiveRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", adaptiveIndexResultDir + "hetero_90th-percentile/", &adaptiveHnsw, k);
+    double averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
     // Heterogeneous 50th percentile queries
     std::cout << "50th percentile heterogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "hetero_50th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "hetero_50th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "hetero_50th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "hetero_50th-percentile/");
 
-    runtimes = postFilterBaselineEvaluateQueries(heteroQueries + "50th-percentile/", baselineResultDir + "hetero_50th-percentile/", postfilterBaseline, k);
-    averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    postfilterRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", postfilterBaselineResultDir + "hetero_50th-percentile/", &postfilterBaseline, k);
+    multiRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", multiFilterBaselineResultDir + "hetero_50th-percentile/", &multiIndexBaseline, k);
+    adaptiveRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", adaptiveIndexResultDir + "hetero_50th-percentile/", &adaptiveHnsw, k);
+    averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
     // Heterogeneous 10th percentile queries
     std::cout << "10th percentile heterogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "hetero_10th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "hetero_10th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "hetero_10th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "hetero_10th-percentile/");
 
-    runtimes = postFilterBaselineEvaluateQueries(heteroQueries + "10th-percentile/", baselineResultDir + "hetero_10th-percentile/", postfilterBaseline, k);
-    averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    postfilterRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", postfilterBaselineResultDir + "hetero_10th-percentile/", &postfilterBaseline, k);
+    multiRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", multiFilterBaselineResultDir + "hetero_10th-percentile/", &multiIndexBaseline, k);
+    adaptiveRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", adaptiveIndexResultDir + "hetero_10th-percentile/", &adaptiveHnsw, k);
+    averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
     // Homogeneous 90th percentile queries
     std::cout << "90th percentile homogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "homo_90th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "homo_90th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "homo_90th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "homo_90th-percentile/");
 
-    runtimes = postFilterBaselineEvaluateQueries(homoQueries + "90th-percentile/", baselineResultDir + "homo_90th-percentile/", postfilterBaseline, k);
-    averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    postfilterRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", postfilterBaselineResultDir + "homo_90th-percentile/", &postfilterBaseline, k);
+    multiRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", multiFilterBaselineResultDir + "homo_90th-percentile/", &multiIndexBaseline, k);
+    adaptiveRuntimes = evaluateQueries(heteroQueries + "90th-percentile/", adaptiveIndexResultDir + "homo_90th-percentile/", &adaptiveHnsw, k);
+    averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
     // Homogeneous 50th percentile queries
     std::cout << "50th percentile homogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "homo_50th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "homo_50th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "homo_50th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "homo_50th-percentile/");
 
-    runtimes = postFilterBaselineEvaluateQueries(homoQueries + "50th-percentile/", baselineResultDir + "homo_50th-percentile/", postfilterBaseline, k);
-    averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    postfilterRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", postfilterBaselineResultDir + "homo_50th-percentile/", &postfilterBaseline, k);
+    multiRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", multiFilterBaselineResultDir + "homo_50th-percentile/", &multiIndexBaseline, k);
+    adaptiveRuntimes = evaluateQueries(heteroQueries + "50th-percentile/", adaptiveIndexResultDir + "homo_50th-percentile/", &adaptiveHnsw, k);
+    averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
     // Homogeneous 10th percentile queries
     std::cout << "10th percentile homogeneous queries" << std::endl;
-    std::filesystem::create_directory(baselineResultDir + "homo_10th-percentile/");
+    std::filesystem::create_directory(postfilterBaselineResultDir + "homo_10th-percentile/");
+    std::filesystem::create_directory(multiFilterBaselineResultDir + "homo_10th-percentile/");
+    std::filesystem::create_directory(adaptiveIndexResultDir + "homo_10th-percentile/");
 
-    runtimes = postFilterBaselineEvaluateQueries(homoQueries + "10th-percentile/", baselineResultDir + "homo_10th-percentile/", postfilterBaseline, k);
-    averageRuntime = average(runtimes);
-    std::cout << "Average runtime: " << averageRuntime << "ms\n" << std::endl;
-    allRuntimes.insert(allRuntimes.end(), runtimes.begin(), runtimes.end());
+    postfilterRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", postfilterBaselineResultDir + "homo_10th-percentile/", &postfilterBaseline, k);
+    multiRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", multiFilterBaselineResultDir + "homo_10th-percentile/", &multiIndexBaseline, k);
+    adaptiveRuntimes = evaluateQueries(heteroQueries + "10th-percentile/", adaptiveIndexResultDir + "homo_10th-percentile/", &adaptiveHnsw, k);
+    averagePostfilterRuntime = average(postfilterRuntimes), averageMultiRuntime = average(multiRuntimes), averageAdaptiveRuntime = average(adaptiveRuntimes);
+    std::cout << "Average postfilter runtime: " << averagePostfilterRuntime << "ms" << std::endl;
+    std::cout << "Average multi-index runtime: " << averageMultiRuntime << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << averageAdaptiveRuntime << "ms\n" << std::endl;
+    allPostfilterRuntimes.insert(allPostfilterRuntimes.end(), postfilterRuntimes.begin(), postfilterRuntimes.end());
+    allMultiRuntimes.insert(allMultiRuntimes.end(), multiRuntimes.begin(), multiRuntimes.end());
+    allAdaptiveRuntimes.insert(allAdaptiveRuntimes.end(), adaptiveRuntimes.begin(), adaptiveRuntimes.end());
 
-    double totalAverageRuntime = average(allRuntimes);
-    std::cout << "Total average runtime: " << totalAverageRuntime << "ms" << std::endl;
+    std::cout << std::endl << "Average postfilter baseline runtime: " << average(allPostfilterRuntimes) << "ms" << std::endl;
+    std::cout << "Average multi-filter index runtime: " << average(allMultiRuntimes) << "ms" << std::endl;
+    std::cout << "Average adaptive index runtime: " << average(allAdaptiveRuntimes) << "ms" << std::endl;
 
     return 0;
 }
